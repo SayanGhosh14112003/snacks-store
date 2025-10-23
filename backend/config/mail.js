fe5c04eb68eec866fe61a8d5e54c57ceb37bc754
SAYAN GHOSH <97933977+SayanGhosh14112003@users.noreply.github.com>
2025-10-23 19:18:41 +0530

    Replace nodemailer with Mailtrap for email sending

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,33 +1,14 @@
-import nodemailer from 'nodemailer';
-
-const sendMail = (to, subject, htmlContent) => {
-  // create transporter
-  const transporter = nodemailer.createTransport({
-    host: 'smtp-relay.brevo.com', // Brevo SMTP host
-    port: 587,                    // port for TLS
-    secure: false,                // false for port 587
-    auth: {
-      user: process.env.MAIL_USER,     // Brevo verified email
-      pass: process.env.MAIL_PASSWORD, // Brevo SMTP key
-    },
-  });
-
-  // mail options
-  const mailOptions = {
-    from: `sayanvk90@gmail.com`, // must match verified email
-    to,
-    subject,
-    html: htmlContent,
-  };
-
-  // send email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('❌ Email sending failed:', error.message);
-    } else {
-      console.log('✅ Email sent:', info.response);
-    }
-  });
-};
-
-export default sendMail;
+import { MailtrapClient } from "mailtrap"
+const client = new MailtrapClient({ token: process.env.MAIL_PASSWORD});
+const sendMail=(to,subject,html)=>{
+const sender = { name: "DEVI SNACKS", email: process.env.MAIL_USER};
+      client.send({
+      from: sender,
+      to:[{email:to}],
+      subject,
+      html,
+    })
+    .then(console.log)
+    .catch(console.error);
+}
+export default sendMail
